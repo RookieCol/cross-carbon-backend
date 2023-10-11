694de0943d40101a54524111769ffea10e44a472
rookiecol <juan.diegam@hotmail.com>
2023-10-11 16:56:38 -0500

    :sparkles: Travel endpoint working

--- src/travel.js
+++ src/travel.js
@@ -2,41 +2,65 @@
 const axios = require("axios");
 
 module.exports.calculate = async (event) => {
+  const { distance, nights } = event.queryStringParameters;
   const apikey = process.env.API_KEY;
-
-  const apiUrl = "https://beta4.api.climatiq.io/estimate";
-
-  // Datos que se enviarán en la solicitud POST
-  const data = {
-    emission_factor: {
-      id: "c334822e-44a8-4cea-b713-23212f8c8f8c",
-    },
-    parameters: {
-      money: 500,
-      money_unit: "usd",
-    },
-  };
-
-  // Configura las cabeceras, incluyendo la Authorization con la variable de entorno API_KEY
+  const url = "https://beta4.api.climatiq.io/estimate"; // Define the URL here
   const headers = {
     Authorization: `Bearer ${apikey}`,
   };
 
-  try {
-    const response = await axios.post(apiUrl, data, { headers });
+  const emissionFactors = {
+    hotelNights: "3e560056-a069-423b-b6a2-fd4825d27969",
+    shortHaul: "d35e42ca-4e87-49a2-a0c3-ca28d8336ec4",
+    mediumHaul: "f4a4f263-1f82-4ae6-a9d7-1b855e7b09b8",
+    longhaul: "ac39995e-72df-4824-a269-b24d24d5c94e",
+  };
+  let selectedEmissionFactorId;
 
-    return {
-      statusCode: response.status,
-      body: JSON.stringify(response.data),
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500, // Puedes ajustar el código de estado según tus necesidades
-      body: JSON.stringify({
-        error: "Error en la solicitud",
-        message: error.message,
-      }),
-    };
+  if (distance < 300) {
+    selectedEmissionFactorId = emissionFactors.shortHaul;
+  } else if (distance >= 300 && distance < 2300) {
+    selectedEmissionFactorId = emissionFactors.mediumHaul;
+  } else {
+    selectedEmissionFactorId = emissionFactors.longhaul;
   }
+
+  const hotelData = {
+    emission_factor: {
+      id: emissionFactors.hotelNights,
+    },
+    parameters: {
+      number: parseInt(nights),
+    },
+  };
+
+  const flightData = {
+    emission_factor: {
+      id: selectedEmissionFactorId,
+    },
+    parameters: {
+      passengers: 1,
+      distance: parseInt(distance),
+      distance_unit: "km",
+    },
+  };
+
+  const hotelEmissionFactor = await axios.post(url, hotelData, { headers });
+  const flightEmissionFactor = await axios.post(url, flightData, { headers });
+
+
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        co2: {
+          hotelEmissionFactor: (hotelEmissionFactor.data.co2e)/1000,
+          flightEmissionFactor: (flightEmissionFactor.data.co2e)/1000,
+        },
+      },
+      null,
+      2
+    ),
+  };
 };
