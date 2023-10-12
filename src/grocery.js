1878b72a4cbaf55db4e753f901830138292df826
rookiecol <juan.diegam@hotmail.com>
2023-10-12 09:56:33 -0500

    :sparkles: Grocery endpoint

--- src/grocery.js
+++ src/grocery.js
@@ -1,31 +1,68 @@
-module.exports.calculate = async (event) => {
-  // Obtenemos los parámetros de consulta de la URL
-  const queryParams = event.queryStringParameters;
+const axios = require("axios");
 
-  if (queryParams) {
-    // Si hay parámetros de consulta, los incluimos en la respuesta
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "Grocery!",
-          input: queryParams, // Incluimos los query parameters en la respuesta
-        },
-        null,
-        2
-      ),
+module.exports.calculate = async (event) => {
+  try {
+    const { proteins, fats, carbs } = event.queryStringParameters;
+    const apikey = process.env.API_KEY;
+    const url = "https://beta4.api.climatiq.io/estimate";
+    const headers = {
+      Authorization: `Bearer ${apikey}`
     };
-  } else {
-    // Si no hay parámetros de consulta, simplemente respondemos con un mensaje
+
+    const emmissionFactors = {
+      proteins: "56184de9-d634-4207-baad-7302e06178cd",
+      fats: "24cf8cbd-9d31-469b-8ec8-7715254d7fbf",
+      carbs: "45bfe641-eff5-497d-9b2f-531bf810f177"
+    };
+
+    const calculateEmission = async (emissionFactorId, amount) => {
+      const data = {
+        emission_factor: {
+          id: emissionFactorId
+        },
+        parameters: {
+          money: parseInt(amount),
+          money_unit: "usd"
+        }
+      };
+
+      const response = await axios.post(url, data, { headers });
+
+      if (response.status !== 200) {
+        throw new Error("Request failed");
+      }
+
+      return (response.data.co2e / 1000).toFixed(4);
+    };
+
+    const [proteinEmissionFactor, fatEmissionFactor, carbEmissionFactor] = await Promise.all([
+      calculateEmission(emmissionFactors.proteins, proteins),
+      calculateEmission(emmissionFactors.fats, fats),
+      calculateEmission(emmissionFactors.carbs, carbs)
+    ]);
+
+    const totalEmission = (parseFloat(proteinEmissionFactor) + parseFloat(fatEmissionFactor) + parseFloat(carbEmissionFactor)).toFixed(4);
+
     return {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: "Grocery! (No query parameters)",
+          co2: {
+            proteins: parseFloat(proteinEmissionFactor),
+            fats: parseFloat(fatEmissionFactor),
+            carbs: parseFloat(carbEmissionFactor),
+            total: parseFloat(totalEmission),
+            units: "Tons"
+          }
         },
         null,
         2
-      ),
+      )
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
     };
   }
 };
